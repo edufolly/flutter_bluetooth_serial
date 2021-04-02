50bc56fc6ac8bad5d5321b43f6e7d41ebcf8d9d6
Ekrem <basari.ekrem@gmail.com>
2021-04-02 15:06:18 +0300

    aa

--- example/lib/AnaModScreen.dart
+++ example/lib/AnaModScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'package:flutter_spleshscreen/Constant/Constant.dart';
+//import 'package:flutter_spleshscreen/Constant/Constant.dart';
 import 'package:flutter/cupertino.dart';
 
 class CustomButton extends StatelessWidget {
