655676ba5bcccaea75f2e11e7253aa92039c823f
Tegar <tegaranugroho@gmail.com>
2023-09-18 14:26:34 +0700

    remove unused string

--- android/src/main/java/io/github/edufolly/flutterbluetoothserial/FlutterBluetoothSerialPlugin.java
+++ android/src/main/java/io/github/edufolly/flutterbluetoothserial/FlutterBluetoothSerialPlugin.java
@@ -278,7 +278,7 @@ public class FlutterBluetoothSerialPlugin implements FlutterPlugin, ActivityAwar
                         discoveryResult.put("name", device.getName());
                         discoveryResult.put("type", device.getType());
                         // discoveryResult.put("class", deviceClass); // @TODO . it isn't my priority for now !BluetoothClass!
-                        discoveryResult.put("isConnected"`, checkIsDeviceConnected(device));
+                        discoveryResult.put("isConnected", checkIsDeviceConnected(device));
                         discoveryResult.put("bondState", device.getBondState());
                         discoveryResult.put("rssi", deviceRSSI);
                         discoveryResult.put("bluetoothClass", device.getBluetoothClass());
